09f61f23d2267c6ce8c3b136e5460b8c140ef863
NicoStellwag <nico.stellwag@gmail.com>
2025-01-23 16:00:24 +0100

    do embedding in batches of 25, works for now but really slow

--- sdk/src/cocosdk/embeddings.py
+++ sdk/src/cocosdk/embeddings.py
@@ -9,7 +9,7 @@ EMBEDDING_URL_BASE = "http://127.0.0.1:8002"
 EMBEDDING_URL = EMBEDDING_URL_BASE
 
 
-def create_embeddings(chunks: List[str]) -> List[List[float]]:
+def create_embeddings(chunks: List[str], batch_size: int = 25) -> List[List[float]]:
     """Create embeddings for a list of chunks using the embedding service.
 
     Args:
@@ -22,13 +22,18 @@ def create_embeddings(chunks: List[str]) -> List[List[float]]:
         List[List[float]]: The embeddings of the chunks.
     """
     headers = {"X-API-Key": API_KEY, "Content-Type": "application/json"}
-    data = json.dumps({"chunks": chunks})
-    embedding_response = call_api(
-        EMBEDDING_URL, "/embed_chunks", method="POST", headers=headers, data=data
-    )
-
-    if not embedding_response["status"] == "success":
-        raise Exception(f"Chunking failed: {embedding_response['error']}")
-
-    embeddings = embedding_response["embeddings"]
+    embeddings = []
+    for i in range(0, len(chunks), batch_size):
+        batch = chunks[i : i + batch_size]
+        data = json.dumps({"chunks": batch})
+        embedding_response = call_api(
+            EMBEDDING_URL,
+            "/embed_chunks",
+            method="POST",
+            headers=headers,
+            data=data,
+        )
+        if not embedding_response["status"] == "success":
+            raise Exception(f"Chunking failed: {embedding_response}")
+        embeddings += embedding_response["embeddings"]
     return embeddings
