d97dbb6c3be45b263b48a08df95668f8152c1859
alex-wiener <alex-wiener@gmx.de>
2025-01-29 17:30:47 +0100

    Added a gradio frontend service

---
+++ services/frontend/app/main.py
@@ -0,0 +1,116 @@
+import json
+import gradio as gr
+import aiohttp
+import requests
+import asyncio
+
+def query_text(text: str, n_results: int = 5) -> str:
+    response = requests.post(
+        "http://localhost:8003/query",
+        headers={
+            "accept": "application/json",
+            "X-API-Key": "test",
+            "Content-Type": "application/json",
+        },
+        json={"text": text, "n_results": n_results},
+    )
+    return "\n".join(f"- {r['document']}\n" for r in response.json()["results"])
+
+
+async def slow_echo(user_message, history):
+    
+    rag_content = query_text(user_message)
+    print(rag_content)
+    system_prompt = """
+    **This is something that came to your mind with regard to the user's last message. Think through if you want to use it.**
+    {{rag_result}}
+    """
+    user_message = f'With this in your mind:\n{rag_content}\n\n{user_message}'
+
+    messages_ollama = [{"role": m['role'], "content": m['content']} for m in history]
+    #messages_ollama.extend([{"role": "system","content": system_prompt.format(rag_result=rag_content)}])
+
+    messages_ollama.append({"role": "user", "content": user_message})
+
+
+    url = "https://jetson-ollama.mitra-labs.ai/api/chat"
+    payload = {
+        "model": "deepseek-r1:14b",
+        "messages": messages_ollama,
+        "stream": True,
+    }
+    
+    thought_buffer = ""
+    response_buffer = ""
+    in_thought = False
+    response_started = False
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url, json=payload) as response:
+                async for line in response.content:
+                    if line:
+                        line = line.decode("utf-8")
+                        try:
+                            data = json.loads(line)
+                            content = data.get("message", {}).get("content", "")
+
+                            if "<think>" in content and not in_thought:
+                                in_thought = True
+                                content = content.replace("<think>", "")
+
+                            if "</think>" in content and in_thought:
+                                in_thought = False                        
+                                response_started = True
+
+                                thought_content = gr.ChatMessage(
+                                        role="assistant",
+                                        content=thought_buffer,
+                                        metadata={"title": "Thinking completed", "status": "done"}
+                                    )
+                                yield thought_content, gr.Textbox(label="",lines = 10,value=rag_content) # more info https://www.gradio.app/docs/gradio/textbox
+
+                            if in_thought:
+                                thought_buffer += content
+                                yield gr.ChatMessage(
+                                        role="assistant",
+                                        content=thought_buffer,
+                                        metadata={"title": "Thinking...", "status": "pending"}
+                                    ) , gr.Textbox(label="",lines = 10,value=rag_content) # more info https://www.gradio.app/docs/gradio/textbox
+
+                            elif response_started:
+                                response_buffer += content
+                                yield [
+                                    thought_content,
+                                    gr.ChatMessage(
+                                        role="assistant",
+                                        content=response_buffer,
+                                    )
+                                    ] , gr.Textbox(label="",lines = 10,value=rag_content) # more info https://www.gradio.app/docs/gradio/textbox
+                                
+                        except json.JSONDecodeError:
+                            print(f"Failed to parse JSON: {line}")
+        except aiohttp.ClientError as e:
+            print(f"Error: {e}")
+
+
+with gr.Blocks(fill_height=True) as demo:
+    
+    rag_reply = gr.Textbox(render=False)
+    gr.Markdown("# A Gradio Chatinterface for CoCo Development")
+    with gr.Row():
+        with gr.Column():
+            #gr.Markdown("### ")
+            gr.ChatInterface(
+                slow_echo,
+                type="messages",
+                #flagging_mode="manual",
+                #save_history=True,
+                #additional_outputs=[rag_reply]
+            )
+        with gr.Column():
+            gr.Markdown("### Reply from RAG System")
+            rag_reply.render()
+    
+if __name__ == "__main__":
+    demo.launch()
