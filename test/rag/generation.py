f35e33596f1c8b1990f6dc1ff8e49799c1b2d764
NicoStellwag <nico.stellwag@gmail.com>
2025-02-14 17:52:03 +0100

    do q and a metrics for retrieved chunks and for mocked chunks including gt and hard negatives

--- test/rag/generation.py
+++ test/rag/generation.py
@@ -14,9 +14,57 @@ from langchain_openai import ChatOpenAI
 from ragas.dataset_schema import SingleTurnSample
 from ragas.metrics import Faithfulness
 from ragas.llms import LangchainLLMWrapper
+import random
+
+from data import unique_texts
 
 
 logger = logging.getLogger(__name__)
+
+
+def mock_top_chunks(ds: Dataset, cfg: DictConfig) -> Dict[str, Dict[str, List]]:
+    """Mock top chunks using gt data and hard negatives.
+    Since only the documents are used, metadata etc. is left as None.
+    Distances are set to 0.0 for gt documents, 0.2 for hard negatives,
+    and 0.5 for random documents.
+
+    Args:
+        ds (Dataset): Dataset to mock top chunks for
+        cfg (DictConfig): Configuration
+
+    Returns:
+        Dict[str, Dict[str, List]]: Mocked top chunks
+    """
+    random.seed(cfg.general.random_seed)
+    top_k = cfg.generation.get_answers.top_k
+    all_documents = [text for text, _ in unique_texts(ds)]
+    top_chunks = {}
+    for sample in ds:
+        query = sample["question"]
+        gt_documents = sample["positive_ctxs"]["text"]
+        hn_documents = sample["hard_negative_ctxs"]["text"]
+
+        documents = gt_documents + hn_documents
+        distances = ([0.0] * len(gt_documents)) + ([0.2] * len(hn_documents))
+
+        # if gt and hard negatives are not enough, add random documents
+        delta_to_top_k = top_k - len(documents)
+        if delta_to_top_k > 0:
+            documents += random.sample(population=all_documents, k=delta_to_top_k)
+            distances += [0.5] * delta_to_top_k
+
+        # if gt and hard negatives are too many, remove some hard negatives
+        if delta_to_top_k < 0:
+            documents = documents[:top_k]
+            distances = distances[:top_k]
+
+        top_chunks[query] = {
+            "ids": [None] * len(documents),
+            "documents": documents,
+            "metadatas": [None] * len(documents),
+            "distances": distances,
+        }
+    return top_chunks
 
 
 def get_answers(
@@ -24,10 +72,13 @@ def get_answers(
     cc: CocoClient,
     cfg: DictConfig,
     wandb_prefix: str,
+    load_from_file: bool,
+    load_file_name: str,
+    output_file_name: str,
 ) -> Dict[str, Dict[str, Any]]:
-    if cfg.generation.get_answers.load_from_file:
+    if load_from_file:
         # load from file if specified
-        answers_file = Path(cfg.generation.get_answers.load_file_name)
+        answers_file = Path(load_file_name)
         with answers_file.open("r") as f:
             answers = json.load(f)
         logger.info(f"Loaded answers from {answers_file}")
@@ -56,7 +107,7 @@ def get_answers(
         logger.info(f"Generated answers with mean tok_s {m_tok_s}")
 
     # save to file
-    output_file = Path(cfg.generation.get_answers.output_file_name)
+    output_file = Path(output_file_name)
     output_file.parent.mkdir(parents=True, exist_ok=True)
     with output_file.open("w") as f:
         json.dump(answers, f)
@@ -208,19 +259,53 @@ def correctness(ds: Dataset, answers: Dict[str, Dict[str, Any]], wandb_prefix: s
 def generation_stage(
     cc: CocoClient, cfg: DictConfig, top_chunks: Dict[str, Dict[str, List]], ds: Dataset
 ) -> None:
-    answers = get_answers(
-        top_chunks=top_chunks, cc=cc, cfg=cfg, wandb_prefix="generation_ret"
+    logger.info("Starting generation stage for retrieved chunks")
+    answers_ret = get_answers(
+        top_chunks=top_chunks,
+        cc=cc,
+        cfg=cfg,
+        wandb_prefix="generation_ret",
+        load_from_file=cfg.generation.get_answers.load_from_file,
+        load_file_name=cfg.generation.get_answers.load_file_name_ret,
+        output_file_name=cfg.generation.get_answers.output_file_name_ret,
     )
     groundedness(
         ds=ds,
-        answers=answers,
+        answers=answers_ret,
         top_chunks=top_chunks,
         cfg=cfg,
         wandb_prefix="generation_ret",
     )
-    relevance(ds=ds, answers=answers)
+    relevance(ds=ds, answers=answers_ret)
     correctness(
         ds=ds,
-        answers=answers,
+        answers=answers_ret,
         wandb_prefix="generation_ret",
     )
+    logger.info("Generation stage for retrieved chunks completed")
+
+    logger.info("Starting generation stage for ground truth chunks")
+    mocked_top_chunks = mock_top_chunks(ds=ds, cfg=cfg)
+    answers_gt = get_answers(
+        top_chunks=mocked_top_chunks,
+        cc=cc,
+        cfg=cfg,
+        wandb_prefix="generation_gt",
+        load_from_file=cfg.generation.get_answers.load_from_file,
+        load_file_name=cfg.generation.get_answers.load_file_name_gt,
+        output_file_name=cfg.generation.get_answers.output_file_name_gt,
+    )
+    groundedness(
+        ds=ds,
+        answers=answers_gt,
+        top_chunks=mocked_top_chunks,
+        cfg=cfg,
+        wandb_prefix="generation_gt",
+    )
+    relevance(ds=ds, answers=answers_gt)
+    correctness(
+        ds=ds,
+        answers=answers_gt,
+        wandb_prefix="generation_gt",
+    )
+    logger.info("Generation stage for ground truth chunks completed")
