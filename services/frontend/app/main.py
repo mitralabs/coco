1cf18f61b14effd9d86a2f688c01147e51b77873
TiMoMuc <tillmann.moeller@tum.de>
2025-02-14 17:03:24 +0100

    working new frontend. pre main merge.

--- services/frontend/app/main.py
+++ services/frontend/app/main.py
@@ -10,7 +10,6 @@ theme = gr.themes.Ocean(
     spacing_size="sm",
 )
 
-
 cc = CocoClient(
     chunking_base="http://chunking:8000",
     db_api_base="http://db-api:8000",
@@ -25,18 +24,49 @@ cc = CocoClient(
 
 # Get available models
 available_models = cc.lm.list_llm_models()
+embedding_models = [
+    "nomic-embed-text:latest",
+    "mxbai-embed-large:latest",
+    "snowflake-arctic-embed:latest",
+    "bge-m3:latest",
+    "all-minilm:latest",
+    "bge-large:latest",
+    "snowflake-arctic-embed2:latest",
+    "paraphrase-multilingual:latest",
+    "granite-embedding:latest",
+]
+available_models = [
+    model for model in available_models if model not in embedding_models
+]
+
 if not available_models:
     available_models = [
         "meta-llama/Llama-3.3-70B-Instruct"
     ]  # Default fallback model for Ollama
 
 
+prompt_template = """ 
+
+{context} 
+"""
+
+system_message_default = "Du bist coco.\n\nDu hilfst dem User bestmöglich.\n\nDu antwortest präzise und kommunizierst auf Deutsch."
+
+CONTEXT_FORMAT = """
+    Eine Recherche in der Datenbank hat folgende Inhalte ergeben:
+
+    -----
+    {context}
+    -----
+"""
+
+
 def user(user_message, history):
-    print(user_message)
+    # print(user_message)
     if history is None:
         history = []
     history.append({"role": "user", "content": user_message})
-    print(history)
+    # print(history)
     return "", history
 
 
@@ -44,20 +74,31 @@ def clear(input_message):
     return ""
 
 
-async def call_rag(user_message, history, selected_model):
+async def call_rag(user_message, history, selected_model, system_message):
     try:
         # Get RAG context
         contexts = await cc.rag.async_retrieve_chunks([user_message], 5)
-        rag_context = contexts[0][1]
-
-        # Format prompt
-        formatted_prompt = cc.rag.format_prompt(user_message, rag_context)
-
-        history.append({"role": "user", "content": user_message})
+        context_chunks = contexts[0][1]
+        rag_context = CONTEXT_FORMAT.format(context="\n-----\n".join(context_chunks))
 
         messages = []
+        # Add system message
+        if system_message == "":
+            system_message = system_message_default
+        messages.append({"role": "system", "content": system_message})
+
+        # Add previous messages
         for element in history:
             messages.append({"role": element["role"], "content": element["content"]})
+
+        # Add RAG context -> Note the "tool" role is ollama specific. See: https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-chat-completion
+        messages.append(
+            {
+                "role": "tool",
+                "content": rag_context,
+            }
+        )
+        print(messages)
 
         responses, tok_ss = await cc.lm.async_chat(
             messages_list=[messages],
@@ -74,7 +115,7 @@ async def call_rag(user_message, history, selected_model):
             [
                 gr.ChatMessage(
                     role="assistant",
-                    content=str(rag_context),
+                    content=rag_context,
                     metadata={"title": "RAG Context", "status": "done"},
                 ),
                 gr.ChatMessage(role="assistant", content=reponse),
@@ -116,7 +157,16 @@ def create_dataframe(query=None):
     if query:
         query_answers = cc.rag.retrieve_chunks(query_texts=[query])
         ids, documents, metadata, distances = query_answers[0]
-        df = df[df["ids"].isin(ids)]
+        df = pd.DataFrame(
+            {
+                "ids": ids,
+                "documents": documents,
+                "filename": [e["filename"] for e in metadata],
+                "distances": [round(e, 2) for e in distances],
+            }
+        )
+        # sort by distance
+        df = df.sort_values(by="distances", ascending=False)
         return df
     else:
         return df
@@ -126,7 +176,8 @@ with gr.Blocks(
     fill_height=True,
     fill_width=True,
     theme=theme,
-    css="#input_message {} footer {visibility: hidden} ",
+    title="coco",
+    css="#input_message {background-color: transparent} footer {visibility: hidden} ",
 ) as demo:
     with gr.Sidebar(open=False):
         gr.Markdown("# ")
@@ -141,7 +192,7 @@ with gr.Blocks(
         system_message = gr.Textbox(
             label="System Message",
             lines=10,
-            placeholder="You are coco.\n\nYou provide insights into data collected by the user.",
+            placeholder=system_message_default,
         )
 
     with gr.Group():
@@ -149,6 +200,8 @@ with gr.Blocks(
             label="coco",
             type="messages",
             height="80vh",
+            # editable="user",
+            render_markdown=False,
         )
         input_message = gr.Textbox(
             placeholder="Type your message here...",
@@ -167,27 +220,23 @@ with gr.Blocks(
                 input_message,
                 chatbot,
                 model_dropdown,
+                system_message,
             ],
             outputs=[chatbot],
         )
 
 with demo.route("Memory") as incrementer_demo:
+
     with gr.Sidebar(open=False):
         gr.Markdown("# ")
         gr.Markdown("# Upload additional data")
         file_upload = gr.File(
-            label="Upload Audio",
+            label="Upload Audio (.wav)",
             file_types=[".wav"],
             type="filepath",
             file_count="single",
         )
         upload_status = gr.Textbox(label="Upload Status", interactive=False)
-        # Add file upload handler
-        file_upload.upload(
-            fn=handle_audio_upload,
-            inputs=[file_upload],
-            outputs=[upload_status],
-        )
 
     query = gr.Textbox(
         label="Query",
@@ -196,6 +245,17 @@ with demo.route("Memory") as incrementer_demo:
     )
 
     data_view = gr.DataFrame(create_dataframe, wrap=True)
+    with gr.Row():
+        btn_show_all = gr.Button("Show All")
+        btn_show_all.click(create_dataframe, outputs=[data_view])
+        gr.Button("Clear Database (Not yet implemented)")
+
+    # Add file upload handler
+    file_upload.upload(
+        fn=handle_audio_upload,
+        inputs=[file_upload],
+        outputs=[upload_status],
+    ).then(create_dataframe, [], [data_view])
 
     query.submit(
         fn=create_dataframe,
@@ -206,11 +266,5 @@ with demo.route("Memory") as incrementer_demo:
 
     # test if change detected. again.
 
-    with gr.Row():
-        btn_show_all = gr.Button("Show All")
-        btn_show_all.click(create_dataframe, outputs=[data_view])
-        gr.Button("Clear Database (Not yet implemented)")
-
-
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(favicon_path="favicon.png")
